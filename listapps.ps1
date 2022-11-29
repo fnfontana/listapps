586e9c16d6767eee128e78ee0e8171f818f0d179
Fernando Fontana <fnfontana18@gmail.com>
2022-11-29 13:51:45 -0300

    Update listapps.ps1
    Added commentary header

--- listapps.ps1
+++ listapps.ps1
@@ -1,3 +1,40 @@
+# Developed by Fernando Fontana under the MIT License, 2022.
+# This means that you can use this script for free, but you can't sell it.
+# Also if you modify it, you must share the modifications under the same license.
+#
+# In other words: You can do whatever you want as long as you include the original copyright
+# and license notice in any copy of the software/source.
+#
+# This script creates the listapps function, that can be used to list all installed programs on the computer.
+# Currently it can get the list from the registry, scoop, winget, chocolatey, pip, pipx, npm, yarn and cargo.
+#
+# It works as wrapper for other commands, so it is not necessary to install it.
+# Just copy the file into your PowerShell profile folder and add the following line to your profile:
+# Invoke-Expression (Get-Content -Path .\Documents\PowerShell\listapps.ps1 -Raw)
+#
+# To see the instructions on how to use this script, run:
+# listapps --help
+#
+# It can be powerful when combined with tools like:
+# - fzf        Powerful Fuzzy Finder to filter the list
+# - ripgrep    Powerful RegEx search on the list
+# - more       Show the list in a pager
+# ...And other commands like peco, grep, findstr, etc.
+#
+# Examples:
+# listapps | fzf
+# listapps | rg -i "python"
+# listapps --choco | fzf
+#
+# Another possibility is to save the list to a file and use it with other commands:
+# listapps --choco > choco_apps.txt
+# listapps --yarn > yarn_packages.txt
+#
+# The cited above functions may be implemented on this script in the future.
+# Or may I develop a whole new cli program with a better interface and more features.
+# But for now, this is a simple script that works well enough for me.
+# =========================================================================================================================================================
+
 # List all programs installed on the computer using the registry
 function show-installed-programs-hklm {
     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher | Sort-Object DisplayName | Format-Table -AutoSize
